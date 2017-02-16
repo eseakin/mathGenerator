c47b5d4d7a1c1fb1d07773712299c10e9e811d96
Eric Eakin <eseakin@gmail.com>
2017-02-15 17:30:17 -0800

    Added ability to answer question. Page returns to input when answer is submitted.

--- src/Generator.js
+++ src/Generator.js
@@ -8,9 +8,18 @@ class Generator extends Component {
       numAnimals: this.randInt(2, 4),
       numFood: this.randInt(1, 3),
       animal: null,
-      many: 'many'
+      many: 'many',
+      answerInput: ''
     };
 
+  }
+
+  handleChange(event) {
+    this.setState({answerInput: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.answer == this.state.answerInput);
   }
 
   randInt(min, max) {
@@ -34,18 +43,27 @@ class Generator extends Component {
 
       this.setState({
         numAnimals: numAnimals * difficulty,
-        numFood: numFood * difficulty
+        numFood: numFood * difficulty,
+        answer: numFood * difficulty * numAnimals * difficulty
       });
+
     }
   }
 
   render() {
     if(this.state.animal !== null) {
       return (
-        <div className="Generator">
-          Difficulty: {this.props.settings.difficulty} <br />
+        <div className="Generator container">
+          Difficulty: {this.props.settings.difficulty} {this.state.answer} {this.state.answerInput}<br /><br />
           There are {this.state.numAnimals} {this.state.animal}s at the zoo. They need to eat {this.state.numFood} {this.state.food} per day to survive. <br />
           How {this.state.many} total {this.state.food} do the {this.state.animal}s eat in a day?
+
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <label className="inputField">
+              <input type="text" value={this.state.answerInput} onChange={this.handleChange.bind(this)} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       );
       
